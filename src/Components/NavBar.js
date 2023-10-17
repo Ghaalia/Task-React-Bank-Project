581b9a5bea092bae749beeb924a59c8237be1930
Ghaalia <Ghaliaals9@gmail.com>
2023-10-17 16:27:14 +0300

    .

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="bg-gray-800 h-20 shadow-2xl shadow-green-800 flex justify-between items-center mb-10">
+      <h1 className=" text-white ml-10">Hello, UserName</h1>
+      <button className="  text-white hover:bg-gray-700 hover:text-red-600 rounded-md px-3 py-2 mr-10">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/Deposit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Deposit = () => {
+  return (
+    <div>
+      <h1 className="text-white text-4xl">Amount to deposit: </h1>
+    </div>
+  );
+};
+
+export default Deposit;
