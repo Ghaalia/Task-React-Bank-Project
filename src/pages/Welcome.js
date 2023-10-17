6b2cad4ae954ee51a6b8577c5c21e8864088a007
lianne <liannealq@hotmail.com>
2023-10-17 18:17:46 +0300

    wel

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,7 +3,10 @@ import React from "react";
 const Welcome = () => {
   return (
     <div>
-      <div>image</div>
+      <div>
+        <h1> WELCOME </h1>
+        <h4> to our BANK</h4>
+      </div>
 
       <div className="flex flex-row">
         <button className=" text-white hover:bg-gray-700 rounded-md px-3 py-2 mr-10 ">
