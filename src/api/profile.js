cd56791422a80fb735ebdff7503eef0ebe563166
Ghaalia <Ghaliaals9@gmail.com>
2023-10-17 19:35:07 +0300

    ..

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,1 +1,30 @@
 import instance from ".";
+
+const getTransactions = () => {
+  const { data } = instance.get("/mini-project/api/transactions/my");
+  return data;
+};
+
+const putDeposit = (amount) => {
+  const { data } = instance.put("/mini-project/api/transactions/deposit");
+  return data;
+};
+
+const putWithdraw = (amount) => {
+  const { data } = instance.put("/mini-project/api/transactions/withdraw");
+  return data;
+};
+
+const putTransfer = (amount, username) => {
+  const { data } = instance.put(
+    `/mini-project/api/transactions/transfer/${username}`
+  );
+  return data;
+};
+
+const getUserInfo = () => {
+  const { data } = instance.get(`/mini-project/api/auth/user/${userID}`);
+  return data;
+};
+
+export { getTransactions, putDeposit, putWithdraw, putTransfer, getUserInfo };
