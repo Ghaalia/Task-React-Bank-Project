af693765038dcee8dffda04c010a9950429b00e1
lianne <liannealq@hotmail.com>
2023-10-17 19:49:01 +0300

    .

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,2 +1,42 @@
 import instance from ".";
 import jwt_decode from "jwt-decode";
+
+const login = async (userInfo) => {
+  const { data } = await instance.post(
+    "/mini-project/api/auth/login",
+    userInfo
+  );
+  storeToken(data?.token);
+  return data;
+};
+
+const register = async (userInfo) => {
+  const formData = new FormData();
+  for (const key in userInfo) formData.append(key, userInfo[key]);
+
+  const { data } = await instance.post(
+    "/mini-project/api/auth/register",
+    formData
+  );
+  storeToken(data?.token);
+  return data;
+};
+
+const storeToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+const checktoken = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const decode = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+    if (decode.exp < currentTime) {
+      localStorage.removeItem("token");
+      return false;
+    } else return true;
+  }
+  return false;
+};
+
+export { login, register, checktoken };
