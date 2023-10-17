1f7bc356c85af083b544da5e809279cc5726eb49
Ghaalia <Ghaliaals9@gmail.com>
2023-10-17 17:54:59 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 import Deposit from "./pages/Deposit";
 import WithDraw from "./pages/WithDraw";
+import Transfer from "./pages/Transfer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
       {/* <Routes>
-        <Route />
-        <Route />
-        
+        <Route path="/Register" Component={Register} />
+        <Route path="/Login" Component={Login} />
+        <Route path="/" Component={HomePage} />
+        <Route path="/Deposit" Component={Deposit} />
+        <Route path="/WithDraw" Component={WithDraw} />
       </Routes> */}
 
       <HomePage />
       <Deposit />
       <WithDraw />
+      <Transfer />
     </div>
   );
 }
