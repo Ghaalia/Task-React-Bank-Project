7bfc58646fb4820b2b5385b02af9f4ab60ea6bd1
lianne <liannealq@hotmail.com>
2023-10-18 18:57:13 +0300

    ...

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
           <div className="flex flex-col justify-center items-center gap-6">
             <h3 className="text-[50px] text-white font-bold">Login</h3>
             <div>
-              <label className="block text-white text-lg font-small">
+              <label className="block text-white text-lg font-small ">
                 Username: {"  "}
                 <input placeholder="" type="text"></input>
               </label>
