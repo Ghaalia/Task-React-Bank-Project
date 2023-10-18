6fdacae506c1a82a0cdf1bc5de8c1eb0ea3e3579
lianne <liannealq@hotmail.com>
2023-10-18 16:27:48 +0300

    ..

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,26 +16,26 @@ const Login = () => {
       <div className=" flex flex-col w-[100%] h-[100%] justify-center items-center gap-5">
         <div className="flex flex-col items-center justify-center gap-20 border border-solid h-[50%] w-[50%]  border-gray-500 border-opacity-20 shadow-lg shadow-green-600 rounded-md ">
           <div className="flex flex-col justify-center items-center gap-6">
-            <h3 className="text-[50px] text-white">Login</h3>
+            <h3 className="text-[50px] text-white font-bold">Login</h3>
             <div>
-              <label className="block text-white text-sm font-small mb-3">
-                Username:
+              <label className="block text-white text-lg font-small">
+                Username: {"  "}
                 <input placeholder="" type="text"></input>
               </label>
             </div>
 
             <div className=" w-[500px] flex justify-center flex-col">
               <div>
-                <label className="block text-white text-sm font-small mb-3">
-                  Username:{"    "}
-                  <input placeholder="" type="text"></input>
+                <label className="block text-white font-small text-lg">
+                  Password:{"    "}
+                  <input placeholder="" type="password"></input>
                 </label>
               </div>
 
               <div className="flex justify-end">
                 <button
                   onClick={handelLogin}
-                  className=" text-white hover:bg-gray-700 rounded-md px-3 py-2 mr-10 "
+                  className=" border border-white text-white hover:bg-gray-700 rounded-md px-3 py-2 mr-10 mt-3"
                 >
                   Login
                 </button>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,40 +12,57 @@ const Register = () => {
   };
   return (
     <div className="flex justify-center h-screen">
-      <div className="  w-[100%] h-[100%] flex flex-col justify-center items-center">
-        <div className="flex flex-col items-center justify-center gap-20 border border-solid h-[50%] w-[50%]  border-gray-500 border-opacity-20 shadow-lg shadow-green-600 rounded-md ">
-          <div className="flex flex-col justify-center items-center gap-6">
-            <h3 className="text-[50px] text-white">Register</h3>
-            <div className="flex flex-col justify-center">
-              <div>
-                <label className="block text-white text-sm font-small mb-3">
-                  Username: <input placeholder="" type="text"></input>
-                </label>
-              </div>
+      <div className="  w-[100%] h-[100%] flex flex-col justify-center items-center ">
+        <div className="flex flex-col items-center justify-center gap-20 border border-solid h-[70%] w-[60%]  border-gray-500 border-opacity-20 shadow-lg shadow-green-600 rounded-md ">
+          <div className="flex flex-col justify-center items-center gap-6 w-full">
+            <h3 className="text-[40px] font-bold text-white">Register</h3>
 
-              <div>
-                <label className="block text-white text-sm font-small mb-3">
-                  Password: <input placeholder="" type="password"></input>
-                </label>
-              </div>
-
-              <div>
-                <label className="block text-white text-sm font-small">
-                  Confirmation: <input placeholder="" type="password"></input>
-                </label>
-              </div>
-              <div>
-                <label className="block text-white text-sm font-small">
-                  Image: <input placeholder="" type="file"></input>
-                </label>
-              </div>
+            <div className="flex justify-between align-middle  w-[60%]">
+              <label className="block text-white text-lg font-small">
+                Username:
+              </label>
+              <input
+                className="w-[250px] px-3 py-1 ml-1"
+                placeholder=""
+                type="text"
+              ></input>
             </div>
-            <div className="flex justify-end">
+
+            <div className="flex justify-between align-middle  w-[60%]">
+              <label className="block text-white text-lg font-small items-start">
+                Password:
+              </label>
+              <input
+                className="w-[250px] px-3 py-1 ml-2"
+                placeholder=""
+                type="password"
+              ></input>
+            </div>
+
+            <div className="flex justify-between align-middle  w-[60%]">
+              <label className=" flex aliblock text-white text-lg font-small">
+                Confirmation:{" "}
+              </label>
+              <input
+                className="w-[250px] px-3 py-1"
+                placeholder=""
+                type="password"
+              ></input>
+            </div>
+
+            <div className="flex justify-between align-middle  w-[60%]">
+              <label className="block text-white text-lg font-small">
+                Image:
+              </label>
+              <input placeholder="" type="file" className="w-[250px] "></input>
+            </div>
+
+            <div>
               <button
                 onClick={handelLogin}
-                className=" text-white hover:bg-gray-700 rounded-md px-3 py-2 mr-10 "
+                className="border border-white border-solid text-white hover:bg-gray-700 rounded-md px-3 py-2 mr-10 "
               >
-                Login
+                Register
               </button>
             </div>
           </div>
